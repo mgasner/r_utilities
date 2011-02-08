6d61d2f30db3e1a53cda8b588b4e62c740deda47
mgasner <max@naviasystems.com>
2011-02-07 16:58:15 -0800

    updated startup script to flag .Rconfig items

--- startup.R
+++ startup.R
@@ -1,6 +1,9 @@
 library(veritable.r)
-Sys.setenv(SWEAVE_STYLEPATH_DEFAULT=TRUE)
 options(veritable.username = "fastmax", veritable.server = "dev.naviasystems.com")
 #options(veritable.username = "fastmax2", veritable.server = "dev.naviasystems.com")
 #options(veritable.username = "veritable", veritable.server = "127.0.0.1")
-setwd("~/")
+
+# don't forget to use setwd() to set the working directory
+
+# what follows belongs in ~/.Rconfig
+Sys.setenv(SWEAVE_STYLEPATH_DEFAULT=TRUE)
